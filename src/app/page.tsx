531a59caa5ff3774b8a86f15dc74c8010c8c66be
Somdev Behera <64832169+somdev-hub@users.noreply.github.com>
2025-01-24 21:56:01 +0530

    implemented hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,51 @@
 import Image from "next/image";
+import { FaCalendar, FaClock } from "react-icons/fa";
+import { MdLocationPin } from "react-icons/md";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="">
+      <div className="mt-[8rem] px-12 flex justify-between items-center">
+        <div className="flex-1">
+          <h2 className="font-lilita-one text-[2.5rem] sub-shadow-head mb-2">
+            Silicon Literary Club presents
+          </h2>
+          <h1>
+            <span className="text-[7.5rem] shadow-head font-lalezar">
+              Model United Nations
+            </span>{" "}
+            <span className="text-[6rem] shadow-head-sec font-lemon">2K25</span>
+          </h1>
+          <h3 className="font-lilita-one text-[2.5rem]">
+            Shaping Tomorrow Through Diplomacy Today.
+          </h3>
+          <button className="font-lilita-one text-white bg-violet rounded-md px-8 mt-4 py-3 text-[1.25rem]">
+            Register Now
+          </button>
+          <div className="flex gap-8 font-lilita-one text-blue mt-6">
+            <div className="flex gap-4 items-center text-[1.75rem]">
+              <FaCalendar />
+              <p>22nd Feb 2025</p>
+            </div>
+            <div className="flex gap-4 items-center text-[1.75rem]">
+              <FaClock />
+              <p>1 pm onwards</p>
+            </div>
+            <div className="flex gap-4 items-center text-[1.75rem]">
+              <MdLocationPin />
+              <p>Gallery 1</p>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <div className="flex-1 flex items-center justify-center">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src={"/assets/hero.svg"}
+            height="600"
+            width="600"
+            alt="Hero Image"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,15 +4,25 @@ export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        blue: "var(--blue)",
+        pink: "var(--pink)",
+        purple: "var(--purple)",
+        violet: "var(--violet)"
       },
-    },
+      fontFamily: {
+        "days-one": "var(--font-days-one)",
+        "lilita-one": "var(--font-lilita-one)",
+        lalezar: "var(--font-lalezar)",
+        lemon: "var(--font-lemon)"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 } satisfies Config;
